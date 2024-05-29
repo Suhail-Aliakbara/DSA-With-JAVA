29932ef4f8f90a18935aa04850af86473931a99e
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-05-29 23:16:32 +0530

    Longest subarray

--- Arrays.java
+++ Arrays.java
@@ -330,4 +330,52 @@ public class Arrays {
   // it will cancell all the element twice hence which element appear once will
   // remain
 
+  /*
+   * Given an array containing N integers and an integer K., Your task is to find
+   * the length of the longest Sub-Array with the sum of the elements equal to the
+   * given value K.
+   * Input :
+   * A[] = {10, 5, 2, 7, 1, 9}
+   * K = 15
+   * Output : 4
+   * Explanation: The sub-array is {5, 2, 7, 1}.
+   * 
+   * Input : A[] = {-1, 2, 3}
+   * K = 6
+   * Output : 0
+   * Explanation: There is no such sub-array with sum 6.
+   */
+
+  // --- This Solution will work for bot Positive and negative---
+  public static int getLongestSubarray(int[] a, long k) {
+    int n = a.length; // size of the array.
+
+    Map<Long, Integer> preSumMap = new HashMap<>();
+    long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+      // calculate the prefix sum till index i:
+      sum += a[i];
+
+      // if the sum = k, update the maxLen:
+      if (sum == k) {
+        maxLen = Math.max(maxLen, i + 1);
+      }
+
+      // calculate the sum of remaining part i.e. x-k:
+      long rem = sum - k;
+
+      // Calculate the length and update maxLen:
+      if (preSumMap.containsKey(rem)) {
+        int len = i - preSumMap.get(rem);
+        maxLen = Math.max(maxLen, len);
+      }
+
+      // Finally, update the map checking the conditions:
+      if (!preSumMap.containsKey(sum)) {
+        preSumMap.put(sum, i);
+      }
+    }
+    return maxLen;
+  }
 }
