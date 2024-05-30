86f5594a79df485c2c2e411e87aec09d4c065432
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-05-30 13:35:07 +0530

    Inverstion Count

--- DivideAndConquer.java
+++ DivideAndConquer.java
@@ -183,8 +183,52 @@ public class DivideAndConquer {
     return count;
   }
 
+  // -------------------------Inversion Count--------------------------
+
+  public static int invertion(int arr[], int si, int ei) {
+    int invCount = 0;
+
+    if (ei > si) {
+      int mid = si + (ei - si) / 2;
+      invCount += invertion(arr, si, mid);
+      invCount += invertion(arr, mid + 1, ei);
+      invCount += mergeInversion(arr, si, mid + 1, ei);
+    }
+    return invCount;
+  }
+
+  public static int mergeInversion(int arr[], int si, int mid, int ei) {
+    int i = si;
+    int j = mid;
+    int k = 0;
+    int invCount = 0;
+
+    int temp[] = new int[ei - si + 1];
+
+    while (i < mid && j <= ei) {
+      if (arr[i] <= arr[j]) {
+        temp[k++] = arr[i++];
+      } else {
+        temp[k] = arr[j];
+        invCount += mid - i;
+        k++;
+        j++;
+      }
+    }
+    while (i < mid) {
+      temp[k++] = arr[i++];
+    }
+    while (j <= ei) {
+      temp[k++] = arr[j++];
+    }
+    for (i = si, k = 0; i <= ei; i++, k++) {
+      arr[i] = temp[k];
+    }
+    return invCount;
+  }
+
   public static void main(String[] args) {
-    int n[] = { 2, 2, 1, 1, 1, 1, 2 };
+    int n[] = { 1, 20, 6, 4, 5 };
     // quickSort(n, 0, n.length - 1);
     // System.out.println(rotatedAndSorted(n, 0, n.length - 1, 2));
     // printFn(n);
@@ -193,6 +237,7 @@ public class DivideAndConquer {
     // String[] arr = mergeArray(str, 0, str.length - 1);
     // printStringArray(arr);
 
-    System.out.println(majorityElement(n, 0, n.length - 1));
+    System.out.println(invertion(n, 0, n.length - 1));
+
   }
 }
