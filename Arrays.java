e048bf6a06d5bfded4fe7bcc2767ed678afdb489
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-05-30 22:33:33 +0530

    Added Two sum problem

--- Arrays.java
+++ Arrays.java
@@ -402,4 +402,65 @@ public class Arrays {
     }
     return maxLen;
   }
+
+  /*
+   * Given an array of integers nums and an integer target, return indices of the
+   * two numbers such that they add up to target.
+   * 
+   * You may assume that each input would have exactly one solution, and you may
+   * not use the same element twice.
+   * Example 1:
+   * 
+   * Input: nums = [2,7,11,15], target = 9
+   * Output: [0,1]
+   * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+   *
+   * Input: nums = [3,2,4], target = 6
+   * Output: [1,2]
+   */
+
+  /*
+   * Time Complexity: O(N), where N = size of the array.
+   * Reason: The loop runs N times in the worst case and searching in a hashmap
+   * takes O(1) generally. So the time complexity is O(N).
+   * 
+   * Note: In the worst case(which rarely happens), the unordered_map takes O(N)
+   * to find an element. In that case, the time complexity will be O(N2). If we
+   * use map instead of unordered_map, the time complexity will be O(N* logN) as
+   * the map data structure takes logN time to find an element.
+   * 
+   * Space Complexity: O(N) as we use the map data structure.
+   */
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> numMap = new HashMap<>();
+    int n = nums.length;
+
+    for (int i = 0; i < n; i++) {
+      int complement = target - nums[i];
+      if (numMap.containsKey(complement)) {
+        return new int[] { numMap.get(complement), i };
+      }
+      numMap.put(nums[i], i);
+    }
+    return new int[] {};
+  }
+
+  // with out using the HashMap TC and SC will be same
+  public static int[] twoSum(int n, int[] arr, int target) {
+    // Arrays.sort(arr);
+    int left = 0, right = n - 1;
+
+    while (left < right) {
+      int sum = arr[left] + arr[right];
+
+      if (sum == target) {
+        return new int[] { left, right };
+      } else if (sum < target)
+        left++;
+      else
+        right--;
+    }
+    return new int[] {};
+  }
+
 }
