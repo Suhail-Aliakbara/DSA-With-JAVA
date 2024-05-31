3c6a4dfc10d73c0b7f93b587671ddee7386ebc85
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-05-31 21:44:58 +0530

    Majority element with Moore Voting and hashMap

--- Arrays.java
+++ Arrays.java
@@ -462,5 +462,55 @@ public class Arrays {
     }
     return new int[] {};
   }
+  /*
+   * Given an array nums of size n, return the majority element.
+   * 
+   * The majority element is the element that appears more than ⌊n / 2⌋ times. You
+   * may assume that the majority element always exists in the array.
+   *
+   * Input: nums = [3,2,3]
+   * Output: 3
+   * 
+   * Input: nums = [2,2,1,1,1,2,2]
+   * Output: 2
+   */
 
+  public int majorityElement(int[] nums) { // TC(O(N))
+    int n = nums.length;
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < n; i++) {
+      map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    }
+
+    n = n / 2;
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if (entry.getValue() > n) {
+        return entry.getKey();
+      }
+    }
+    return 0;
+  }
+  // Approach 3: Moore Voting Algorithm
+  // The intuition behind the Moore's Voting Algorithm is based on the fact that
+  // if there is a majority element in an array, it will always remain in the
+  // lead, even after encountering other elements.
+
+  public int majorityElement2(int[] nums) {
+    int count = 0;
+    int candidate = 0;
+
+    for (int num : nums) {
+      if (count == 0) {
+        candidate = num;
+      }
+
+      if (num == candidate) {
+        count++;
+      } else {
+        count--;
+      }
+    }
+    return candidate;
+  }
 }
