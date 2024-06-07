e2c81a247402d9090ec5e866a9c8629e699e571d
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-06-07 23:07:10 +0530

    Majority Element n/3

--- hashMap.java
+++ hashMap.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -296,5 +297,125 @@ public class hashMap {
       mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
     }
     return cnt;
+  }
+
+  /*
+   * Given an integer array of size n, find all elements that appear more than ⌊
+   * n/3 ⌋ times.
+   * 
+   * Input: nums = [3,2,3]
+   * Output: [3]
+   * 
+   * Input: nums = [1]
+   * Output: [1]
+   * 
+   * Input: nums = [1,2]
+   * Output: [1,2]
+   */
+
+  // Better Approach
+  /*
+   * Time Complexity: O(N*logN), where N = size of the given array.
+   * 
+   * Reason: We are using a map data structure. Insertion in the map takes logN
+   * time. And we are doing it for N elements. So, it results in the first term
+   * O(N*logN).
+   * If we use unordered_map instead, the first term will be O(N) for the best and
+   * average case and for the worst case, it will be O(N2).
+   * 
+   * Space Complexity: O(N) as we are using a map data structure. We are also
+   * using a list that stores a maximum of 2 elements. That space used is so small
+   * that it can be considered constant.
+   */
+
+  public static List<Integer> majorityElement3(int[] nums) {
+    int n = nums.length; // size of the array
+    List<Integer> ls = new ArrayList<>(); // list of answers
+
+    // declaring a map:
+    HashMap<Integer, Integer> mpp = new HashMap<>();
+
+    // least occurrence of the majority element:
+    int mini = (int) (n / 3) + 1;
+
+    // storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+      int value = mpp.getOrDefault(nums[i], 0);
+      mpp.put(nums[i], value + 1);
+
+      // checking if nums[i] is
+      // the majority element:
+      if (mpp.get(nums[i]) == mini) {
+        ls.add(nums[i]);
+      }
+      if (ls.size() == 2)
+        break;
+    }
+
+    return ls;
+  }
+
+  // Optimal Approach
+
+  /*
+   * Time Complexity: O(N) + O(N), where N = size of the given array.
+   * 
+   * Reason: The first O(N) is to calculate the counts and find the expected
+   * majority elements. The second one is to check if the calculated elements are
+   * the majority ones or not.
+   * 
+   * Space Complexity: O(1) as we are only using a list that stores a maximum of 2
+   * elements. The space used is so small that it can be considered constant.
+   */
+
+  public static List<Integer> majorityElement4(int[] v) {
+    int n = v.length; // size of the array
+
+    int cnt1 = 0, cnt2 = 0; // counts
+    int el1 = Integer.MIN_VALUE; // element 1
+    int el2 = Integer.MIN_VALUE; // element 2
+
+    // applying the Extended Boyer Moore's Voting Algorithm:
+    for (int i = 0; i < n; i++) {
+      if (cnt1 == 0 && el2 != v[i]) {
+        cnt1 = 1;
+        el1 = v[i];
+      } else if (cnt2 == 0 && el1 != v[i]) {
+        cnt2 = 1;
+        el2 = v[i];
+      } else if (v[i] == el1)
+        cnt1++;
+      else if (v[i] == el2)
+        cnt2++;
+      else {
+        cnt1--;
+        cnt2--;
+      }
+    }
+
+    List<Integer> ls = new ArrayList<>(); // list of answers
+
+    // Manually check if the stored elements in
+    // el1 and el2 are the majority elements:
+    cnt1 = 0;
+    cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+      if (v[i] == el1)
+        cnt1++;
+      if (v[i] == el2)
+        cnt2++;
+    }
+
+    int mini = (int) (n / 3) + 1;
+    if (cnt1 >= mini)
+      ls.add(el1);
+    if (cnt2 >= mini)
+      ls.add(el2);
+
+    // Uncomment the following line
+    // if it is told to sort the answer array:
+    // Collections.sort(ls); //TC --> O(2*log2) ~ O(1);
+
+    return ls;
   }
 }
