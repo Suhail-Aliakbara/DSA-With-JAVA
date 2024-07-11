8899de381fab4988bd9d3b4f033e60ee256e43a7
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-07-11 21:03:45 +0530

    Job sequencing problem

--- greedy.java
+++ greedy.java
@@ -24,6 +24,16 @@ class itemComparator implements Comparator<Item> { // class for Fractional Knaps
   }
 }
 
+class Job { // class for Job Sequencing Problem
+  int id, profit, deadline;
+
+  Job(int x, int y, int z) {
+    this.id = x;
+    this.deadline = y;
+    this.profit = z;
+  }
+}
+
 public class greedy {
 
   /*
@@ -156,6 +166,49 @@ public class greedy {
       }
     }
     return finalvalue;
+  }
+
+  /*
+   * Job Sequencing Problem
+   * Given a set of N jobs where each jobi has a deadline and profit associated
+   * with it.
+   * Each job takes 1 unit of time to complete and only one job can be scheduled
+   * at a time. We earn the profit associated with job if and only if the job is
+   * completed by its deadline.
+   * Find the number of jobs done and the maximum profit.
+   * 
+   * Input: N = 4
+   * Jobs = {(1,4,20),(2,1,10),(3,1,40),(4,1,30)}
+   * Output: 2 60
+   * Explanation:
+   * Job1 and Job3 can be done with
+   * maximum profit of 60 (20+40).
+   */
+
+  int[] JobScheduling(Job arr[], int n) {
+    // Your code here
+    Arrays.sort(arr, (a, b) -> (b.profit - a.profit));
+    int maxDeadline = -1;
+
+    for (int i = 0; i < n; i++) {
+      maxDeadline = Math.max(maxDeadline, arr[i].deadline);
+    }
+
+    int temp[] = new int[maxDeadline + 1];
+    int maxProfit = 0;
+    int day = 0;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = arr[i].deadline - 1; j >= 0; j--) {
+        if (temp[j] == 0) {
+          maxProfit += arr[i].profit;
+          temp[j] = arr[i].id;
+          day++;
+          break;
+        }
+      }
+    }
+    return new int[] { day, maxProfit };
   }
 
 }
