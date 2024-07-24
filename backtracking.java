183876e67865d167a72b4f48354a51b643af5b58
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-07-24 13:51:35 +0530

    sudoku solver condition

--- backtracking.java
+++ backtracking.java
@@ -190,4 +190,19 @@ public class backtracking {
     nQueens(n, 0, ans, board, upperCol, leftUpperDiagonal, rightUpperDiagonal);
     return ans;
   }
+
+  /*
+   * Sudoku solver codition with only 1 loop
+   * boolean isSafe(char[][] board, int row,int col, char digit){
+   * for(int i=0; i<9; i++){
+   * if(board[row][i] == digit) return false;
+   * if(board[i][col] == digit) return false;
+   * 
+   * if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == digit) return
+   * false;
+   * }
+   * return true;
+   * }
+   */
+
 }
