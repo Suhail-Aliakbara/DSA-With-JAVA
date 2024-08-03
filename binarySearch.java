6193c66761d847ed934b497ab96efa06e7048048
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-08-03 17:48:55 +0530

    Single element in sorted array

--- binarySearch.java
+++ binarySearch.java
@@ -86,4 +86,39 @@ public class binarySearch {
    * Space Complexity : O(1)
    */
 
+  /*
+   * 540. Single Element in a Sorted Array
+   * You are given a sorted array consisting of only integers where every element
+   * appears exactly twice, except for one element which appears exactly once.
+   * Return the single element that appears only once.
+   * Your solution must run in O(log n) time and O(1) space.
+   * Input: nums = [3,3,7,7,10,11,11]
+   * Output: 10
+   */
+
+  public int singleNonDuplicate(int[] arr) {
+    int n = arr.length;
+
+    int low = 0;
+    int high = n - 2;
+
+    while (low <= high) {
+      int mid = (low + high) / 2;
+
+      if (arr[mid] == arr[mid ^ 1]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+    return arr[low];
+  }
+  /*
+   * 540 ->
+   * if i am in left half -> 1st instance == even && 2nd instance == odd of
+   * currElement
+   * if i am in right half -> 1st instance == odd && 2nd instance == even of
+   * currElement
+   */
+
 }
