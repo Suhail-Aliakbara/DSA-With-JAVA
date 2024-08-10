4ce03d46b30b74d1409a8e068ad4249da6cac79b
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-08-10 06:44:44 +0530

    median of two sorted array

--- binarySearch.java
+++ binarySearch.java
@@ -161,4 +161,73 @@ public class binarySearch {
     return -1;
   }
 
+  /*
+   * 4. Median of Two Sorted Arrays
+   * Input: nums1 = [1,3], nums2 = [2]
+   * Output: 2.00000
+   * Explanation: merged array = [1,2,3] and median is 2.
+   * 
+   * Input: nums1 = [1,2], nums2 = [3,4]
+   * Output: 2.50000
+   * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+   */
+
+  public static double median(int[] a, int[] b) {
+    // Size of two given arrays
+    int n1 = a.length;
+    int n2 = b.length;
+
+    int n = n1 + n2; // total size
+    // required indices:
+    int ind2 = n / 2;
+    int ind1 = ind2 - 1;
+    int cnt = 0;
+    int ind1el = -1, ind2el = -1;
+
+    // apply the merge step:
+    int i = 0, j = 0;
+    while (i < n1 && j < n2) {
+      if (a[i] < b[j]) {
+        if (cnt == ind1)
+          ind1el = a[i];
+        if (cnt == ind2)
+          ind2el = a[i];
+        cnt++;
+        i++;
+      } else {
+        if (cnt == ind1)
+          ind1el = b[j];
+        if (cnt == ind2)
+          ind2el = b[j];
+        cnt++;
+        j++;
+      }
+    }
+
+    // copy the left-out elements:
+    while (i < n1) {
+      if (cnt == ind1)
+        ind1el = a[i];
+      if (cnt == ind2)
+        ind2el = a[i];
+      cnt++;
+      i++;
+    }
+    while (j < n2) {
+      if (cnt == ind1)
+        ind1el = b[j];
+      if (cnt == ind2)
+        ind2el = b[j];
+      cnt++;
+      j++;
+    }
+
+    // Find the median:
+    if (n % 2 == 1) {
+      return (double) ind2el;
+    }
+
+    return (double) ((double) (ind1el + ind2el)) / 2.0;
+  }
+
 }
