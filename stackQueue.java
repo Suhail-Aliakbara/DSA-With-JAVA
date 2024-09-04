7dd1772905d21f6c2ab1b080bb9b5a6c70e554a2
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-09-04 19:54:15 +0530

    implement stack using 1 queue

--- stackQueue.java
+++ stackQueue.java
@@ -1,6 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class stackQueue {
+
+  /*
+   * Implementing Stack using Queues
+   */
+  // Using 1 queue
+  Queue<Integer> queue = new LinkedList<>();
+
+  public void push(int x) {
+    queue.add(x);
+    for (int i = 0; i < queue.size() - 1; i++) {
+      queue.add(queue.poll());
+    }
+  }
+
+  public int pop() {
+    return queue.poll();
+  }
+
+  public int top() {
+    return queue.peek();
+  }
+
+  public boolean empty() {
+    return queue.isEmpty();
+  }
+
+  //
+
   /*
    * 20. Valid Parentheses
    * Given a string s containing just the characters '(', ')', '{', '}', '[' and
