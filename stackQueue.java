185a61ae9c673c4ae6162458976b1646ed8754f3
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-10-02 11:55:22 +0530

    LRU cache problem

--- stackQueue.java
+++ stackQueue.java
@@ -172,4 +172,79 @@ public class stackQueue {
     }
     return arr;
   }
+
+  /*
+   * LRU cache
+   * Design a data structure that follows the constraints of a Least Recently Used
+   * (LRU) cache.
+   * 
+   * Implement the LRUCache class:
+   * 
+   * //LRUCache(int capacity) Initialize the LRU cache with positive size
+   * capacity.
+   * //int get(int key) Return the value of the key if the key exists, otherwise
+   * return -1.
+   * //void put(int key, int value) Update the value of the key if the key exists.
+   * Otherwise, add the key-value pair to the cache. If the number of keys exceeds
+   * the capacity from this operation, evict the least recently used key.
+   * 
+   * The functions get and put must each run in O(1) average time complexity.
+   */
+  class LRUCache {
+
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
+    Map<Integer, Node> map = new HashMap<>();
+    int capacity = 0;
+
+    public LRUCache(int _capacity) {
+      capacity = _capacity;
+      head.next = tail;
+      tail.prev = head;
+    }
+
+    public int get(int key) {
+      if (!map.containsKey(key))
+        return -1;
+      Node node = map.get(key);
+      deleteNode(node);
+      insertAtFront(node);
+
+      return node.value;
+    }
+
+    public void put(int key, int value) {
+      if (map.containsKey(key))
+        deleteNode(map.get(key));
+      else if (map.size() == capacity)
+        deleteNode(tail.prev);
+      insertAtFront(new Node(key, value));
+    }
+
+    public void deleteNode(Node node) {
+      map.remove(node.key);
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+
+    public void insertAtFront(Node node) {
+      map.put(node.key, node);
+      node.next = head.next;
+      head.next.prev = node;
+      head.next = node;
+      node.prev = head;
+    }
+  }
+
+  public class Node {
+    int key, value;
+    Node next, prev;
+
+    Node(int key, int value) {
+      this.key = key;
+      this.value = value;
+      this.next = null;
+      this.prev = null;
+    }
+  }
 }
