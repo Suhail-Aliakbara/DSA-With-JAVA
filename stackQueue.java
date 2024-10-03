7fc26e0a4ed99d092d1e0697a41bd223fb67c063
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-10-03 16:46:11 +0530

    largest Rectangle Area OPTIMAL 1

--- stackQueue.java
+++ stackQueue.java
@@ -250,6 +250,8 @@ public class stackQueue {
 
   /*
    * 460. LFU Cache
+   * Design and implement a data structure for a Least Frequently Used (LFU)
+   * cache.
    * 
    * Implement the LFUCache class:
    *
@@ -378,4 +380,56 @@ public class stackQueue {
     }
   }
 
+  /*
+   * 84. Largest Rectangle in Histogram
+   * Given an array of integers heights representing the histogram's bar height
+   * where the width of each bar is 1, return the area of the largest rectangle in
+   * the histogram.
+   * Input: heights = [2,1,5,6,2,3]
+   * Output: 10
+   * Explanation: The above is a histogram where width of each bar is 1.
+   * The largest rectangle is shown in the red area, which has an area = 10 units.
+   */
+
+  public int largestRectangleArea(int[] heights) {
+    int n = heights.length;
+    Stack<Integer> st = new Stack<>();
+    int leftSmall[] = new int[n];
+    int rightSmall[] = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        st.pop();
+      }
+      if (st.isEmpty())
+        leftSmall[i] = 0;
+      else
+        leftSmall[i] = st.peek() + 1;
+
+      st.push(i);
+    }
+
+    while (!st.isEmpty())
+      st.pop();
+
+    for (int i = n - 1; i >= 0; i--) {
+      while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        st.pop();
+      }
+      if (st.isEmpty())
+        rightSmall[i] = n - 1;
+      else
+        rightSmall[i] = st.peek() - 1;
+
+      st.push(i);
+    }
+
+    int maxRect = 0;
+
+    for (int i = 0; i < n; i++) {
+      maxRect = Math.max(maxRect, heights[i] * (rightSmall[i] - leftSmall[i] + 1));
+    }
+
+    return maxRect;
+  }
 }
