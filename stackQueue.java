afd24ffc74946943ede35c04029886b38e9d6b94
Suhail-Aliakbara <suhailaliakbara99@gmail.com>
2024-10-05 13:35:26 +0530

    994. Rotting Oranges

--- stackQueue.java
+++ stackQueue.java
@@ -497,6 +497,13 @@ public class stackQueue {
     }
     return maxx;
   }
+  /*
+   * Time Complexity: O(N^2)
+   * Reason: One loop for traversing and another to findMax
+   * 
+   * Space Complexity: O(K)
+   * Reason: No.of windows
+   */
 
   public int[] maxSlidingWindow1(int[] nums, int k) { // OPTIMAL- TC:O(N) SC:O(K)
 
@@ -519,5 +526,69 @@ public class stackQueue {
       }
     }
     return arr;
+  }
+  /*
+   * 994. Rotting Oranges
+   * You are given an m x n grid where each cell can have one of three values:
+   * 
+   * 0 representing an empty cell,
+   * 1 representing a fresh orange, or
+   * 2 representing a rotten orange.
+   * 
+   * Every minute, any fresh orange that is 4-directionally adjacent to a rotten
+   * orange becomes rotten.
+   * Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+   * Output: 4
+   */
+
+  public int orangesRotting(int[][] grid) {
+    if (grid == null || grid.length == 0)
+      return 0;
+    int rows = grid.length;
+    int cols = grid[0].length;
+    Queue<int[]> queue = new LinkedList<>();
+    int count_fresh = 0;
+    // Put the position of all rotten oranges in queue
+    // count the number of fresh oranges
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] == 2) {
+          queue.offer(new int[] { i, j });
+        }
+        if (grid[i][j] != 0) {
+          count_fresh++;
+        }
+      }
+    }
+
+    if (count_fresh == 0)
+      return 0;
+    int countMin = 0, cnt = 0;
+    int dx[] = { 0, 0, 1, -1 };
+    int dy[] = { 1, -1, 0, 0 };
+
+    // bfs starting from initially rotten oranges
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      cnt += size;
+      for (int i = 0; i < size; i++) {
+        int[] point = queue.poll();
+        for (int j = 0; j < 4; j++) {
+          int x = point[0] + dx[j];
+          int y = point[1] + dy[j];
+
+          if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 ||
+              grid[x][y] == 2)
+            continue;
+
+          grid[x][y] = 2;
+          queue.offer(new int[] { x, y });
+        }
+      }
+      if (queue.size() != 0) {
+        countMin++;
+      }
+    }
+    return count_fresh == cnt ? countMin : -1;
   }
 }
